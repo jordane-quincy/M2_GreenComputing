d6519f29d5b21b4f7c6395780cfefa0a120bbb8f
jordane-quincy <jordanequincy@gmail.com>
2017-04-15 11:02:57 +0200

    [fix] l'application ne se lance plus suite au push

--- M2_GreenComputing/app/src/main/java/com/github/jordane_quincy/m2_greencomputing/MainActivity.java
+++ M2_GreenComputing/app/src/main/java/com/github/jordane_quincy/m2_greencomputing/MainActivity.java
@@ -1,31 +1,23 @@
 package com.github.jordane_quincy.m2_greencomputing;
 
-import android.app.Notification;
-import android.app.PendingIntent;
 import android.content.Intent;
-import android.support.design.widget.TabLayout;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -74,14 +66,15 @@ public class MainActivity extends AppCompatActivity {
         });
 
         /////////////////////////////////SetData Activity//////////////////////////////////
-        Button decrease_sound_button = (Button) findViewById(R.id.decrease_sound_button);
-        decrease_sound_button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                TextView info_text = (TextView) findViewById(R.id.info_text);
-                info_text.setText("decrease");
-            }
-        });
+//        FIXME: @MaD : l'activity crash si ce bout de code est décommenté.
+//        Button decrease_sound_button = (Button) findViewById(R.id.decrease_sound_button);
+//        decrease_sound_button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                TextView info_text = (TextView) findViewById(R.id.info_text);
+//                info_text.setText("decrease");
+//            }
+//        });
         Button increase_sound_button = (Button) findViewById(R.id.increase_sound_button);
         Button decrease_lights_button = (Button) findViewById(R.id.decrease_lights_button);
         Button increase_lights_button = (Button) findViewById(R.id.increase_lights_button);
