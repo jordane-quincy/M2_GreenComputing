e8198d0f3d2d5ba214a01415f6d2de8900d561ac
jordane-quincy <jordanequincy@gmail.com>
2017-04-15 16:15:04 +0200

    [evo] lancement du service au démarrage du téléphone

--- M2_GreenComputing/app/src/main/java/com/github/jordane_quincy/m2_greencomputing/RecordService.java
+++ M2_GreenComputing/app/src/main/java/com/github/jordane_quincy/m2_greencomputing/RecordService.java
@@ -1,14 +1,20 @@
 package com.github.jordane_quincy.m2_greencomputing;
 
+import android.app.ActivityManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Binder;
+import android.os.Debug;
 import android.os.IBinder;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by jordane on 06/04/17.
@@ -16,22 +22,18 @@ import android.widget.Toast;
 
 public class RecordService extends Service {
 
+    private static final String TAG = RecordService.class.getSimpleName();
+    // This is the object that receives interactions from clients.  See
+    // RemoteService for a more complete example.
+    private final IBinder mBinder = new LocalBinder();
+    private final int MILLE_VINGT_QUATRE = 1024;
+    private final int BYTES_TO_MO = MILLE_VINGT_QUATRE * MILLE_VINGT_QUATRE;
+    ActivityManager activitymanager;
+    Debug.MemoryInfo memoryInfo;
     private NotificationManager mNM;
-
     // Unique Identification Number for the Notification.
     // We use it on Notification start, and to cancel it.
     private int NOTIFICATION = 42;//R.string.local_service_started;
-
-    /**
-     * Class for clients to access.  Because we know this service always
-     * runs in the same process as its clients, we don't need to deal with
-     * IPC.
-     */
-    public class LocalBinder extends Binder {
-        RecordService getService() {
-            return RecordService.this;
-        }
-    }
 
     @Override
     public void onCreate() {
@@ -50,13 +52,31 @@ public class RecordService extends Service {
 
         startForeground(42, notification);
 
+        initLogging();
+
         // Display a notification about us starting.  We put an icon in the status bar.
         showNotification();
+
+//        OneSignal.appContext = this.getApplicationContext();
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//
+//                OneSignal.appId = OneSignal.getSavedAppId();
+//
+//                OneSignalStateSynchronizer.initUserState(OneSignal.appContext);
+//                OneSignalStateSynchronizer.syncUserState(true);
+//                checkOnFocusSync();
+//
+//                stopSelf();
+//            }
+//        }, "OS_SYNCSRV_ONCREATE").start();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Log.i("RecordService", "Received start id " + startId + ": " + intent);
+        Log.i(TAG, "Received start id " + startId + ": " + intent);
+        launchLogging();
         return START_NOT_STICKY;
     }
 
@@ -73,10 +93,6 @@ public class RecordService extends Service {
     public IBinder onBind(Intent intent) {
         return mBinder;
     }
-
-    // This is the object that receives interactions from clients.  See
-    // RemoteService for a more complete example.
-    private final IBinder mBinder = new LocalBinder();
 
     /**
      * Show a notification while this service is running.
@@ -103,5 +119,65 @@ public class RecordService extends Service {
 
         // Send the notification.
         mNM.notify(NOTIFICATION, notification);
+    }
+
+    private void initLogging() {
+        Log.d(TAG, "initLogging");
+
+        //TODO: trouver un autre moyen de montrer que le service a bien été lancé.
+        openWebPage("http://www.google.com?q=" + Calendar.getInstance().getTimeInMillis());
+        activitymanager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
+
+    }
+
+    private void launchLogging() {
+        Log.d(TAG, "launchLogging");
+
+//        ActivityManager activitymanager = (ActivityManager)getSystemService(ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> lstRunningApp = activitymanager.getRunningAppProcesses();
+
+//        sb.append(runningApp.processName);
+        for (ActivityManager.RunningAppProcessInfo runningApp : lstRunningApp) {
+            if (ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND == runningApp.importance) {
+                int[] pids = {runningApp.pid};
+                Debug.MemoryInfo[] memoryInfos = activitymanager.getProcessMemoryInfo(pids);
+                memoryInfo = memoryInfos[0];
+//                Log.d(TAG, convertKbToMo(memoryInfo.getTotalPrivateDirty()) +", "+ convertKbToMo(memoryInfo.getTotalPss()) +", "+ convertKbToMo(memoryInfo.getTotalPrivateClean()));
+                break;
+            }
+
+        }
+
+//        RAM
+        ActivityManager activityManager = (ActivityManager) this.getSystemService(ACTIVITY_SERVICE);
+        ActivityManager.MemoryInfo memoryInfo = new ActivityManager.MemoryInfo();
+        activityManager.getMemoryInfo(memoryInfo);
+
+        Log.d(TAG, convertbytesToMo(memoryInfo.availMem) + "," + convertbytesToMo(memoryInfo.totalMem) + "," + convertbytesToMo(memoryInfo.totalMem - memoryInfo.availMem));
+
+    }
+
+    private long convertbytesToMo(long bytes) {
+        return bytes / BYTES_TO_MO;
+    }
+
+    private void openWebPage(String url) {
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); //imperatif car on est dans un service (useless dans une activity)
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+    }
+
+    /**
+     * Class for clients to access.  Because we know this service always
+     * runs in the same process as its clients, we don't need to deal with
+     * IPC.
+     */
+    public class LocalBinder extends Binder {
+        RecordService getService() {
+            return RecordService.this;
+        }
     }
 }
